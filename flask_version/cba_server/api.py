eaae6bc3ac2152e9bd1a5158144964dab1ec6a1e
Jamie Cook <jamie.cook@veitchlister.com.au>
2021-09-11 18:55:38 +1000

    steel threaded

--- flask_version/cba_server/api.py
+++ flask_version/cba_server/api.py
@@ -1,49 +1,57 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for, jsonify
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    url_for,
+    jsonify,
 )
 from werkzeug.exceptions import abort
 import pandas as pd
+import math
 
 from cba_server.db import get_db
-from roads_cba_py.section import Section, InvalidSection
+from roads_cba_py.section import Section, InvalidSection, parse_section
 from roads_cba_py.cba import CostBenefitAnalysisModel
+from roads_cba_py.utils import flatten, split_on_condition
 
-bp = Blueprint('api', __name__)
 
-@bp.route('/run_sections', methods=['POST'])
+bp = Blueprint("api", __name__)
+
+
+@bp.route("/run_sections", methods=["POST"])
 def run_sections():
-    valid_sections, invalid_sections,stats = split_assets_by_validity(request.json)
+    valid_sections, invalid_sections, stats = split_assets_by_validity(request.json)
+
     cba = CostBenefitAnalysisModel()
     results = [cba.compute_cba_for_section(s) for s in valid_sections]
 
-    return jsonify({ "stats": stats, "invalids": invalid_sections, "data": [s.to_primitive() for s in results] })
+    valid_results, invalid_results = split_on_condition(results, lambda r: not math.isnan(r.eirr))
 
-@bp.route('/evaluate_assets', methods=['POST'])
+    problems = flatten([s.invalid_reason() for s in invalid_sections]) + ["eirr is NaN"] * len(invalid_results)
+    invalid_reasons = pd.DataFrame(data=problems, columns=["reason"])
+    invalid_reasons = invalid_reasons["reason"].value_counts().to_dict()
+
+    return jsonify(
+        {
+            "stats": stats,
+            "invalids": invalid_reasons,
+            "data": [s.to_primitive() for s in valid_results],
+        }
+    )
+
+
+@bp.route("/evaluate_assets", methods=["POST"])
 def evaluate_assets():
-    valid_sections, invalid_sections,stats = split_assets_by_validity(request.json)
-    return jsonify({ "stats": stats, "invalids": invalid_sections })
+    _valid_sections, invalid_sections, stats = split_assets_by_validity(request.json)
+    return jsonify({"stats": stats, "invalids": invalid_sections})
 
-@bp.route('/jamie', methods=['GET'])
-def jamie():
-    return jsonify({"answer": 42})
 
-def parse_section(json):
-    try:
-        return Section(json)
-    except Exception as err:
-        return InvalidSection([str(err)], json)
-
-def split_assets_by_validity(data): 
+def split_assets_by_validity(data):
     sections = [parse_section(s) for s in data]
-    sections = [(s,s.invalid_reason()) for s in sections]
+    valid_sections, invalid_sections = split_on_condition(sections, lambda s: s.invalid_reason() is None)
 
-    valid_sections = [s for (s,r) in sections if r is None]
-    invalid_sections = [(s,r) for (s,r) in sections if r is not None]
-
-    stats = {"valid": len(valid_sections), "invalid": len(invalid_sections) }
-    invalids = pd.DataFrame(data=flatten([r for (s,r) in invalid_sections]), columns=['reason'])
-    invalids = invalids['reason'].value_counts().to_dict()
-    return (valid_sections, invalids, stats)
- 
-def flatten(a_of_a):
-    return [x for y in a_of_a for x in y]
+    stats = {"valid": len(valid_sections), "invalid": len(invalid_sections)}
+    return (valid_sections, invalid_sections, stats)
--- flask_version/tests/test_api.py
+++ flask_version/tests/test_api.py
@@ -1,1 +1,15 @@
-.
+import unittest
+from os.path import join, dirname
+import sys
+
+import os
+
+sys.path.append(".")
+print(os.getcwd())
+print(__file__)
+
+
+class TestApi(unittest.TestCase):
+    def test_simple(self):
+        s = {"id": 7}
+        self.assertEqual("635950_304", s.id)
