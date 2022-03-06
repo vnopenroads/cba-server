a2074fe7f418be81085b8c7e85708ab4e86138ba
Jamie Cook <jamie.cook@veitchlister.com.au>
2022-03-06 13:04:03 +1000

    MISSING FILE!!!! SO MUCH TIME WASTED!!!!

--- cba_server/__init__.py
+++ cba_server/__init__.py
@@ -1,19 +1,20 @@
 import os
 from flask import Flask
-import cba_server.db
-import cba_server.api
+import cba_server.db as db
+import cba_server.api as api
+
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
